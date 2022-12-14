a8f933eb61f1aeaf263066af80e2ddefd8294b81
Deepeshgiri <52114069+Deepeshgiri@users.noreply.github.com>
2022-12-15 02:22:54 +0530

    test

---
+++ java practice/printerScenario.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class printerScenario {
+
+
+    public static void main(String [] args) {
+        //initializing possible scenario
+        String a = "print";
+        String b= "cancel";
+        //person start
+        Scanner sc = new Scanner(System.in);
+        System.out.println("type 'print' to take print out,\n type 'cancel' to abort");
+        //input from person
+        String input = sc.next();
+
+        // computer taking instruction
+        if (a.equals(input)) {
+            //computer communicating with printer
+            System.out.println("your print here");
+            // computer taking instruction
+        } else if (b.equals(input)) {
+            //computer communicating with printer
+            System.out.println("print command aborted");
+        } else {
+            //computer giving feedback to person
+            System.out.println("invalid command please try again");
+        }
+
+    }
+}
