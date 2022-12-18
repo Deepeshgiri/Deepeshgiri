b8a827927992a0a515d527257f5862c6882b4ba5
Deepesh kumar <52114069+Deepeshgiri@users.noreply.github.com>
2022-12-19 00:11:14 +0530

    print number

--- java practice/printer.java
+++ java practice/printer.java
@@ -1,10 +1,92 @@
-public class printer {
-    public String print(){
+/* class printer {
+     print x = print.getInstance();
+     print y = load_paper.getInstance();
 
-        computer obj = new computer();
-        String task = obj.logic();
-       // System.out.println(task);
-        return task;
-    }
+     if
 
 }
+ class PrinterFactory{
+    public static PrinterTask( String type, String task){
+        if (type == "print"){return print;
+            else (type == "error"){
+                return load_paper;
+            }
+    }
+ }
+
+ */
+
+
+
+// Java program implementing Singleton class
+// with using getInstance() method
+
+// Class 1
+// Helper class
+class Singleton {
+    // Static variable reference of single_instance
+    // of type Singleton
+    private static Singleton single_instance = null;
+
+    // Declaring a variable of type String
+    public String s;
+
+    // Constructor
+    // Here we will be creating private constructor
+    // restricted to this class itself
+    private Singleton()
+    {
+        s = "Hello I am a string part of Singleton class";
+    }
+
+    // Static method
+    // Static method to create instance of Singleton class
+    public static Singleton getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Singleton();
+
+        return single_instance;
+    }
+}
+/*
+// Class 2
+// Main class
+class GFG {
+    // Main driver method
+    public static void main(String args[])
+    {
+        // Instantiating Singleton class with variable x
+        Singleton x = Singleton.getInstance();
+
+        // Instantiating Singleton class with variable y
+        Singleton y = Singleton.getInstance();
+
+        // Instantiating Singleton class with variable z
+        Singleton z = Singleton.getInstance();
+
+        // Printing the hash code for above variable as
+        // declared
+        System.out.println("Hashcode of x is "
+                + x.hashCode());
+        System.out.println("Hashcode of y is "
+                + y.hashCode());
+        System.out.println("Hashcode of z is "
+                + z.hashCode());
+
+        // Condition check
+        if (x == y && y == z) {
+
+            // Print statement
+            System.out.println(
+                    "Three objects point to the same memory location on the heap i.e, to the same object");
+        }
+
+        else {
+            // Print statement
+            System.out.println(
+                    "Three objects DO NOT point to the same memory location on the heap");
+        }
+    }
+}
+*/
---
+++ pattern/PrintNumber.java
@@ -0,0 +1,12 @@
+public class PrintNumber {
+
+    public static void main(String args []){
+
+        for (int i =1; i<=5; i++){
+            System.out.println("");
+            for (int j =1; j<=5; j++){
+                System.out.print(j);
+            }
+        }
+    }
+}
