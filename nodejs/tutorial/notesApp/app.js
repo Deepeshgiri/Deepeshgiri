5ff3c29331ce5af9cdf1aee3e51283ae5603fc55
Deepesh kumar <52114069+Deepeshgiri@users.noreply.github.com>
2023-01-14 01:16:06 +0530

    sdak

--- nodejs/tutorial/notesApp/app.js
+++ nodejs/tutorial/notesApp/app.js
@@ -1,4 +1,49 @@
-const fs =require('fs')
-const getNotes = require('./notes.js')
-const name=require('./utils.js')
-console.log(getNotes)
+const chalk = require('chalk')
+const { parse } = require('path')
+const validator = require('validator')
+const { describe } = require('yargs')
+const yargs = require('yargs')
+
+
+yargs.command({
+    command:"add",
+    describe:"adding note",
+    handler: function(){
+        console.log('add note to see')
+    }
+})
+
+yargs.command({
+    command:"remove",
+    describe:"removing note",
+    handler: function(){
+        console.log('deleting note')
+    }
+})
+
+yargs.command({
+    command:"list",
+    describe:"listing note",
+    builder:{
+        title:{
+            describe:"all notes",
+           demandOption:true,
+            type:"string"
+        }
+    },
+    handler: function(argv){
+        console.log('Title >> '+ argv.title)
+    }
+})
+
+yargs.command({
+    command:"read",
+    describe:"reading note",
+    handler: function(){
+        console.log('read specific note')
+    }
+})
+yargs.parse()
+// console.log(process.argv)
+// console.log(yargs.argv)
+
--- nodejs/tutorial/notesApp/utils.js
+++
@@ -1,7 +0,0 @@
-const name= 'Deepesh giri'
-const add = function(a,b){
-
-    return a+b
-} 
-module.exports=add
-console.log('utils.js')
