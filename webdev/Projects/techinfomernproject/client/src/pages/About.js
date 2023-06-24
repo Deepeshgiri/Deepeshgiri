a1c3f0db6c1009f6957f5956fa7a8e56b6d4d78c
Deepesh kumar <52114069+Deepeshgiri@users.noreply.github.com>
2023-06-24 15:10:50 +0530

    frontend

---
+++ webdev/Projects/techinfomernproject/client/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+export const About = () => {
+  return (
+ <Layout>
+    <h1>About page</h1>
+ </Layout>
+  )
+}
---
+++ webdev/Projects/techinfomernproject/client/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+export const HomePage = () => {
+  return (
+    <Layout>
+        <h1>Homepage</h1>
+    </Layout>
+  )
+}
