c72d20c18843bed56d8a76c42033bdc4ef3e626f
unknown <dr.deepeshgiri@gmail.com>
2024-01-02 23:46:01 +0530

    login page

--- MERNstack/ChatApp/chat-client/src/components/account/LoginDilog.js
+++ MERNstack/ChatApp/chat-client/src/components/account/LoginDilog.js
@@ -1,11 +1,24 @@
-import {Dialog} from '@mui/material'
+import {Box, Dialog, List, ListItem} from '@mui/material'
 
 const LoginDilog = ()=>{
     return(
-        <>
-        
-        </>
-        
+        <Box>
+        <Dialog open={true}> 
+        <Box>
+                <h2>Use WhatsApp on your computer</h2>
+                <List>
+                <ListItem>Open WhatsApp on your phone</ListItem>
+                <ListItem>Tap Menu Or Settings and select linked Device</ListItem>
+                <ListItem>tap on <strong>Link a device</strong></ListItem>
+                <ListItem>Point your phone to this screen to capture the QR code</ListItem>
+                </List>
+                
+            </Box>
+            <Box>
+                <h1>Qr code</h1>
+            </Box>
+        </Dialog>
+        </Box>
     )
 }
 export default LoginDilog
