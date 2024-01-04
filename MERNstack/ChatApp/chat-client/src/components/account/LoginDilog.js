63b0154f5e0f545b9b36f99b332754b48ce5ad0d
unknown <dr.deepeshgiri@gmail.com>
2024-01-04 23:27:24 +0530

    form

--- MERNstack/ChatApp/chat-client/src/components/account/LoginDilog.js
+++ MERNstack/ChatApp/chat-client/src/components/account/LoginDilog.js
@@ -32,13 +32,26 @@ const LoginDilog = ()=>{
                 <Box><Button>Login</Button><Button onClick={SignUp}>SignUp</Button></Box>
             </Box>
         </Dialog>
-        <FormGroup>
-            <Input placeholder='Email'>Email </Input>
-            <Input placeholder='password'>Password</Input>
-            <Button variant="outlined" size="medium">
-          Medium
-        </Button>
-        </FormGroup>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
         </dialogBox>
     )
 }
