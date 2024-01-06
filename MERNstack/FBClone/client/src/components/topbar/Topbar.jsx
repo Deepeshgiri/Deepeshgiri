28778e1d58b032fb1ba5b0b0b569f73e213c8bf3
unknown <dr.deepeshgiri@gmail.com>
2024-01-06 21:50:43 +0530

    sapp

---
+++ MERNstack/FBClone/client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import './topbar.css'
+import {} from '@mui/icons-material'
+export default function(){
+    return(
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className='logo'>Fearbook</span>
+            </div>
+            <div className="topbarcenter">
+                <div className="searchbar">
+                    
+                </div>
+            </div>
+            <div className="topbarLeft"></div>
+        </div>
+    )
+}
---
+++ MERNstack/FBClone/client/src/pages/home/Home.js
@@ -0,0 +1,10 @@
+
+
+import PersonIcon from '@mui/icons-material/Person';
+
+export default function Home  (){
+return(
+    <PersonIcon/>
+)
+}
+
