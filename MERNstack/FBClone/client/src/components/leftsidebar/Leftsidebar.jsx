77cd8634d163f293997856a792f96362fbb32ac7
unknown <dr.deepeshgiri@gmail.com>
2024-01-08 23:19:29 +0530

    leftbar

--- MERNstack/FBClone/client/src/components/leftsidebar/Leftsidebar.jsx
+++ MERNstack/FBClone/client/src/components/leftsidebar/Leftsidebar.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import './leftsidebar.css'
+import { Bookmark, Camera, Chat, Group, Person, PlayCircleFilledOutlined, School, Work } from '@mui/icons-material';
+import { Button } from '@mui/material';
 
 const Leftsidebar = () => {
   return (
     <div className='leftsidebar'>
-        Leftsidebar
+        
+
+        <div className="wrapper" >
+            <ul>
+                <li>
+                <Chat/>
+                Chat
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <PlayCircleFilledOutlined/>
+                <span>Videos</span>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <School/>
+                <span>Courses</span>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <Work/>
+                <span>Jobs</span>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <Group/>
+                <span>Groups</span>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <Bookmark/>
+                <span>Bookmark</span>
+                </li>
+            </ul>
+            
+            
+            
+           
+           
+           
+
+            <Button>Show More</Button>
+            
+        </div>
         </div>
   )
 }
