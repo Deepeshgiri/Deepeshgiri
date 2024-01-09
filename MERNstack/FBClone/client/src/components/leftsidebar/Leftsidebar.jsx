d14bbd47608f8db8bc9dd8826f903cb57cce0f12
unknown <dr.deepeshgiri@gmail.com>
2024-01-09 21:45:19 +0530

    left side

--- MERNstack/FBClone/client/src/components/leftsidebar/Leftsidebar.jsx
+++ MERNstack/FBClone/client/src/components/leftsidebar/Leftsidebar.jsx
@@ -1,62 +1,116 @@
-import React from 'react'
-import './leftsidebar.css'
-import { Bookmark, Camera, Chat, Group, Person, PlayCircleFilledOutlined, School, Work } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import React from "react";
+import "./leftsidebar.css";
+import {
+  Bookmark,
+  Camera,
+  Chat,
+  Group,
+  Person,
+  PlayCircleFilledOutlined,
+  School,
+  Work,
+} from "@mui/icons-material";
+import { Button } from "@mui/material";
 
 const Leftsidebar = () => {
   return (
-    <div className='leftsidebar'>
-        
+    <div className="leftsidebar">
+      <div className="wrapper">
+        <ul>
+          <li>
+            <Chat />
+            <span>Chat</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <PlayCircleFilledOutlined />
+            <span>Videos</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <School />
+            <span>Courses</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Work />
+            <span>Jobs</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Group />
+            <span>Groups</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Bookmark />
+            <span>Bookmark</span>
+          </li>
+        </ul>
 
-        <div className="wrapper" >
-            <ul>
-                <li>
-                <Chat/>
-                Chat
-                </li>
-            </ul>
-            <ul>
-                <li>
-                <PlayCircleFilledOutlined/>
-                <span>Videos</span>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                <School/>
-                <span>Courses</span>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                <Work/>
-                <span>Jobs</span>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                <Group/>
-                <span>Groups</span>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                <Bookmark/>
-                <span>Bookmark</span>
-                </li>
-            </ul>
-            
-            
-            
-           
-           
-           
+        <Button>Show More</Button>
+        <div className="bottomleftbar">
+        <ul className="bottomlist">
+        <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          <li className="btmlistitem">
+            <img src="assets/bloomd.jpg" alt="img" className="imageleftbar" />
+            <span className="personname">name of person</span>
+          </li>
+          
+        </ul>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
 
-            <Button>Show More</Button>
-            
-        </div>
-        </div>
-  )
-}
-
-export default Leftsidebar
+export default Leftsidebar;
