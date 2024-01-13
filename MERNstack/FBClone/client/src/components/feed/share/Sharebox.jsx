3434ac52ee7f308be94b26c4f209c64f7de54e8b
unknown <dr.deepeshgiri@gmail.com>
2024-01-13 19:58:34 +0530

    feed share box

--- MERNstack/FBClone/client/src/components/feed/share/Sharebox.jsx
+++ MERNstack/FBClone/client/src/components/feed/share/Sharebox.jsx
@@ -6,11 +6,13 @@ export const Sharebox = () => {
   return (
     <div className='sharewrapper'>
         <div className="box">
-            <input type="text" className="shareinputtext" placeholder="what's in your mind"></input>
+            <img src="assets/discbot.jpg" alt="" className="profileimg" ></img>
+            
+            <textarea name="" id="" cols="70" rows="6" className="shareinputtext"  placeholder="what's in your mind"></textarea>
             <div className="shareicons">
-                    <PinDrop></PinDrop>
-                    <Collections/>
-                    <EmojiEmotions/>
+                    <PinDrop className="shareicon"/><span>Location</span>
+                    <Collections className="shareicon"/><span>Photo or Video</span>
+                    <EmojiEmotions className="shareicon"/><span>Feeling or activity</span>
                 </div>
 
         </div>
