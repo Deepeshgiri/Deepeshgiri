16e06fe8d6b6e4df4b6cdaac704aff925d4a25f5
unknown <dr.deepeshgiri@gmail.com>
2024-01-13 20:32:13 +0530

    box shadow

--- MERNstack/FBClone/client/src/components/feed/feed.jsx
+++ MERNstack/FBClone/client/src/components/feed/feed.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './feed.css'
 import { Sharebox } from './share/Sharebox'
+import Feedcontent from './feedcontent/Feedcontent'
 
 const feed = () => {
   return (
   <div className='feed'>
     <Sharebox/>
+    <Feedcontent/>
   </div>
   )
 }
---
+++ MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './feedcontent.css'
+
+const Feedcontent = () => {
+  return (
+    <div className="photovideo">
+        <div className="content">
+            <img src="./assets/bloomd.jpg" alt="" className='imagevideo' />
+        </div>
+    </div>
+  )
+}
+
+export default Feedcontent
