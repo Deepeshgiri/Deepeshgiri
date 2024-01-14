28dbee17851e7907e7b021ada88198759a32ad6c
unknown <dr.deepeshgiri@gmail.com>
2024-01-14 19:49:18 +0530

    pics

--- MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
+++ MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
@@ -5,7 +5,24 @@ const Feedcontent = () => {
   return (
     <div className="photovideo">
         <div className="content">
-            <img src="./assets/bloomd.jpg" alt="" className='imagevideo' />
+            <img src="./assets/14.jpeg" alt="" className='imagevideo' />
+        </div>
+
+        <div className="content">
+            <img src="./assets/13.jpeg" alt="" className='imagevideo' />
+        </div>
+        <div className="content">
+            <img src="./assets/12.jpeg" alt="" className='imagevideo' />
+        </div>
+
+        <div className="content">
+            <img src="./assets/16.jpeg" alt="" className='imagevideo' />
+        </div>
+        <div className="content">
+            <img src="./assets/17.jpeg" alt="" className='imagevideo' />
+        </div>
+        <div className="content">
+            <img src="./assets/18.jpeg" alt="" className='imagevideo' />
         </div>
     </div>
   )
