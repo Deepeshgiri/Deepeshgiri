f0e1cb846ce4dc7dd04794bea69e585b62c2bc37
unknown <dr.deepeshgiri@gmail.com>
2024-01-15 23:00:58 +0530

    feed post

--- MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
+++ MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
@@ -3,26 +3,35 @@ import './feedcontent.css'
 
 const Feedcontent = () => {
   return (
-    <div className="photovideo">
-        <div className="content">
-            <img src="./assets/14.jpeg" alt="" className='imagevideo' />
-        </div>
+    // <div className="photovideo">
+    //     <div className="content">
+    //         <img src="./assets/14.jpeg" alt="" className='imagevideo' />
+    //     </div>
 
-        <div className="content">
-            <img src="./assets/13.jpeg" alt="" className='imagevideo' />
-        </div>
-        <div className="content">
-            <img src="./assets/12.jpeg" alt="" className='imagevideo' />
-        </div>
+    //     <div className="content">
+    //         <img src="./assets/13.jpeg" alt="" className='imagevideo' />
+    //     </div>
+    //     <div className="content">
+    //         <img src="./assets/12.jpeg" alt="" className='imagevideo' />
+    //     </div>
 
-        <div className="content">
-            <img src="./assets/16.jpeg" alt="" className='imagevideo' />
-        </div>
-        <div className="content">
-            <img src="./assets/17.jpeg" alt="" className='imagevideo' />
-        </div>
-        <div className="content">
-            <img src="./assets/18.jpeg" alt="" className='imagevideo' />
+    //     <div className="content">
+    //         <img src="./assets/16.jpeg" alt="" className='imagevideo' />
+    //     </div>
+    //     <div className="content">
+    //         <img src="./assets/17.jpeg" alt="" className='imagevideo' />
+    //     </div>
+    //     <div className="content">
+    //         <img src="./assets/18.jpeg" alt="" className='imagevideo' />
+    //     </div>
+    // </div>
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postLeft">
+                    <img src="./assets/discbot.jpg" alt="" className="postimg" />
+                </div>
+            </div>
         </div>
     </div>
   )
