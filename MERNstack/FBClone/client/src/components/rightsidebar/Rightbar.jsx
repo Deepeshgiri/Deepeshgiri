df431638115d61b91eea109ddefaf615a5351191
unknown <dr.deepeshgiri@gmail.com>
2024-01-19 01:55:22 +0530

    rightbar

--- MERNstack/FBClone/client/src/components/rightsidebar/Rightbar.jsx
+++ MERNstack/FBClone/client/src/components/rightsidebar/Rightbar.jsx
@@ -6,9 +6,11 @@ const Rightbar = () => {
     <div className='rightbar'>
       <div className="rightBarWrapper">
         <div className="birthdayContainer">
-          {/* <img src="./assets/18.jpeg" alt="" className="bdayimage" /> */}
-          <span className="bdaytext">{" "} xyz and 32 others have birthday today</span>
+          <img src="./assets/18.jpeg" alt="" className="bdayimage" />
+          <span className="bdaytext"> xyz and 32 others have birthday today</span>
         </div>
+        <img src="./assets/hire-me.png" alt="" className="rightbarAd" />
+        <marquee behavior="" direction="" className="adtext">Ad goes here</marquee>
       </div>
     </div>
   )
