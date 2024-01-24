4b90e2903e71db2e46fad5814456b90337f6fe86
unknown <dr.deepeshgiri@gmail.com>
2024-01-24 21:39:37 +0530

    fixing share

--- MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
+++ MERNstack/FBClone/client/src/components/feed/feedcontent/Feedcontent.jsx
@@ -12,28 +12,7 @@ const Feedcontent = () => {
         borderRadius: '5px',
     }
   return (
-    // <div className="photovideo">
-    //     <div className="content">
-    //         <img src="./assets/14.jpeg" alt="" className='imagevideo' />
-    //     </div>
-
-    //     <div className="content">
-    //         <img src="./assets/13.jpeg" alt="" className='imagevideo' />
-    //     </div>
-    //     <div className="content">
-    //         <img src="./assets/12.jpeg" alt="" className='imagevideo' />
-    //     </div>
-
-    //     <div className="content">
-    //         <img src="./assets/16.jpeg" alt="" className='imagevideo' />
-    //     </div>
-    //     <div className="content">
-    //         <img src="./assets/17.jpeg" alt="" className='imagevideo' />
-    //     </div>
-    //     <div className="content">
-    //         <img src="./assets/18.jpeg" alt="" className='imagevideo' />
-    //     </div>
-    // </div>
+   
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
